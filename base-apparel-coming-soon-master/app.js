dbc412b4fc57c224ab4af9fb9769d64947ea1958
Enzo Nascimento <dev.enzody@gmail.com>
2024-01-19 10:46:35 -0300

    new project

---
+++ base-apparel-coming-soon-master/app.js
@@ -0,0 +1,19 @@
+function validateEmail () {
+    var email = document.getElementById("email");
+    var regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!regexEmail.test(email.value)) {
+        var alerta = document.querySelector(".alerta")
+        alerta.style.display = "block"
+
+        var box = document.getElementById("email-box")
+        box.style.border = "1px solid hsl(0, 93%, 68%)"
+
+        var error = document.getElementById("error")
+        error.style.display = "block"
+
+        return false
+    } else {
+        return true
+    }
+}
