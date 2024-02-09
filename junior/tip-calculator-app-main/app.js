54cdb9abfc38bf3dfa846f14c4cb196444a013ae
Enzo Nascimento <dev.enzody@gmail.com>
2024-02-09 15:39:47 -0300

    ATT

--- junior/tip-calculator-app-main/app.js
+++ junior/tip-calculator-app-main/app.js
@@ -6,27 +6,101 @@ const total = document.querySelector("#total");
 
 document.getElementById('bill-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    
-    alert(`Valor de ${bill.value} para ${numPeople.value} pessoas | valor de gorgeta: ${customTip.value}`);
-
-    if (bill.value !== "" && numPeople.value !== "" && customTip.value !== "") {
-        alert(`Valor de ${bill.value} para ${numPeople.value} pessoas | valor de gorgeta: ${customTip.value}`);
-    }
-
-    return;
 });
 
-// Adiciona eventos de escuta para cada input usando 'input'
-bill.addEventListener("input", verificarInputs);
-numPeople.addEventListener("input", verificarInputs);
-customTip.addEventListener("input", verificarInputs);
+// Verifica o retorno das entradas
+function checkInputs() {
 
-function verificarInputs() {
-    console.log("alterou");
-    if (bill.value !== "" && numPeople.value !== "" && customTip.value !== "") {
-        // Faça o que for necessário quando todos os campos estiverem preenchidos
+    var checkButtons = 0
+    var tipAmountPerc = 0
+    var isValid = false
+
+    if (customTip.value !== "") {
+        disableAllButtons()
     }
+
+    buttons.forEach(button => {
+        if (button.classList.contains("active")) {
+            checkButtons = button.value
+        }
+    })
+
+    
+
+    if (checkButtons !== 0) {
+
+        if (bill.value !== "" && numPeople.value !== "") {
+            tipAmountPerc = checkButtons
+            isValid = true
+        }
+
+    } else if (bill.value !== "" && numPeople.value !== "" && 
+    customTip.value !== "") {
+
+        tipAmountPerc = customTip.value
+        isValid = true
+    }
+    
+    if (isValid) {
+        var billMap = {
+            "bill": Number(bill.value),
+            "numPeople": Number(numPeople.value),
+            "tipAmountPerc": Number(tipAmountPerc)
+        }
+
+        
+        /*  PAREI AQUI, ARRUMANDO A LOGICA PARA MOSTRAR OS RESULTADOS
+
+        total.value = (billMap.bill / billMap.numPeople)
+        tipAmount.value = total.value * (billMap.tipAmountPerc / 100)
+        total.value -= tipAmount.value
+        console.log(billMap.bill / billMap.numPeople)
+        console.log(total.value * (billMap.tipAmountPerc / 100))
+        console.log(tipAmount)
+        */
+    
+        return console.log(billMap) 
+    }
+
+    return false
 }
+
+// Disparo dos Inputs
+const inputs = document.querySelectorAll('input');
+inputs.forEach(input => {
+    input.addEventListener("input", () => {
+        checkInputs()
+    });
+});
+
+// Disparo dos Buttons
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        buttons.forEach(otherButton => {
+            if (otherButton !== button) {
+                otherButton.classList.remove("active");
+            }
+        });
+
+            if (customTip.value !== "") {
+                customTip.value = ""
+            }
+
+            button.classList.toggle("active");
+            checkInputs()     
+    });
+});
+
+// Desativa todos os Buttons
+function disableAllButtons() {
+    buttons.forEach(otherButton => {
+        otherButton.classList.remove("active");            
+    })
+}
+
+
 
 
 
