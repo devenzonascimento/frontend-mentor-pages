c1c6716a2f01b42a273c272f5d3393993f1edd9f
Enzo Nascimento <dev.enzody@gmail.com>
2024-02-09 23:51:59 -0300

    new project

--- junior/tip-calculator-app-main/app.js
+++ junior/tip-calculator-app-main/app.js
@@ -3,7 +3,12 @@ const numPeople = document.querySelector("#n-people");
 const customTip = document.querySelector("#custom-tip");
 const tipAmount = document.querySelector("#tip-amount");
 const total = document.querySelector("#total");
+const resetButton = document.querySelector("#reset-btn")
 
+const inputs = document.querySelectorAll('input');
+const buttons = document.querySelectorAll('button');
+
+// Evita o envio automatico do formulario
 document.getElementById('bill-form').addEventListener('submit', function(event) {
     event.preventDefault();
 });
@@ -15,25 +20,24 @@ function checkInputs() {
     var tipAmountPerc = 0
     var isValid = false
 
+    // Se a % custom for inserida, ele desconsidera os botões
     if (customTip.value !== "") {
         disableAllButtons()
     }
 
+    // Se tiver algum botão ativo, guardo a o valor do botão
     buttons.forEach(button => {
         if (button.classList.contains("active")) {
             checkButtons = button.value
         }
     })
 
-    
 
-    if (checkButtons !== 0) {
+    if (checkButtons !== 0 && bill.value !== "" && numPeople.value !== "") {
 
-        if (bill.value !== "" && numPeople.value !== "") {
-            tipAmountPerc = checkButtons
-            isValid = true
-        }
-
+        tipAmountPerc = checkButtons
+        isValid = true
+        
     } else if (bill.value !== "" && numPeople.value !== "" && 
     customTip.value !== "") {
 
@@ -41,40 +45,55 @@ function checkInputs() {
         isValid = true
     }
     
+    // Caso seja valido, e todos os inputs forem recebidos, guarda os valores em um objeto
     if (isValid) {
         var billMap = {
             "bill": Number(bill.value),
-            "numPeople": Number(numPeople.value),
-            "tipAmountPerc": Number(tipAmountPerc)
+            "tipAmountPerc": Number(tipAmountPerc),
+            "numPeople": Number(numPeople.value)
         }
 
-        
-        /*  PAREI AQUI, ARRUMANDO A LOGICA PARA MOSTRAR OS RESULTADOS
-
-        total.value = (billMap.bill / billMap.numPeople)
-        tipAmount.value = total.value * (billMap.tipAmountPerc / 100)
-        total.value -= tipAmount.value
-        console.log(billMap.bill / billMap.numPeople)
-        console.log(total.value * (billMap.tipAmountPerc / 100))
-        console.log(tipAmount)
-        */
-    
-        return console.log(billMap) 
+        showResults(billMap)
+        activatedResetButton()
     }
 
-    return false
+    return
+}
+
+// Verifica se o valor dos inputs esta no formato Number
+function validateInputs(input) {
+    if (input.value !== "" && isNaN(Number(input.value))) {      
+        input.style.border = "2px solid lightcoral"
+    } else {
+        input.style.border = ""
+    }
+}
+
+// Ativa o uso do Reset Button, para resetar o form
+function activatedResetButton() {
+    resetButton.classList.add("active")
+    resetButton.disabled = false
+
+}
+
+// Exibe os resultados na tela
+function showResults(billMap) {
+    var tipAmountValue = (billMap.bill / billMap.numPeople) * (billMap.tipAmountPerc / 100);
+    tipAmount.textContent = tipAmountValue.toFixed(2);
+    
+    var totalValue = (billMap.bill / billMap.numPeople) + tipAmountValue;
+    total.textContent = totalValue.toFixed(2);
 }
 
 // Disparo dos Inputs
-const inputs = document.querySelectorAll('input');
 inputs.forEach(input => {
     input.addEventListener("input", () => {
         checkInputs()
+        validateInputs(input)
     });
 });
 
 // Disparo dos Buttons
-const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener("click", () => {
 
@@ -93,12 +112,59 @@ buttons.forEach(button => {
     });
 });
 
+// Mensagem de erro do elemento NumPeople
+numPeople.addEventListener("input", () => {
+
+    if (Number(numPeople.value) == 0) {      
+        errorStyles(numPeople)
+    } else {
+        validateInputs(numPeople)
+        removeStyles(numPeople)
+    }
+})
+
+// Ações ao disparar o Reset Button
+resetButton.addEventListener("click", () => {
+
+    resetButton.classList.remove("active")
+    disableAllButtons()
+    tipAmount.textContent = "$0.00"
+    total.textContent = "$0.00"
+
+    
+})
+
 // Desativa todos os Buttons
 function disableAllButtons() {
     buttons.forEach(otherButton => {
         otherButton.classList.remove("active");            
     })
 }
+
+// Adiciona os estilo CSS de erro no elemento
+function errorStyles(input) {
+
+    removeStyles(input)
+
+    var errorElement = document.createElement("p")
+    errorElement.classList.add("msg-error")
+    errorElement.textContent = "Can't be zero"
+
+    input.style.border = "2px solid lightcoral"
+    input.closest(".input-box").appendChild(errorElement)
+}
+
+// Remove os estilo CSS de erro no elemento
+function removeStyles(input) {
+    var errorElement = input.parentNode.parentNode.querySelector(".msg-error")
+
+    if (errorElement) {
+        errorElement.remove()
+    }
+}
+
+
+
 
 
 
