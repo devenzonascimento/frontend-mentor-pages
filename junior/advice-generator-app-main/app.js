a7fb4d7c05cba1a591b118b380fb61d7c475fdaf
Enzo Nascimento <dev.enzody@gmail.com>
2024-02-12 21:57:52 -0300

    new project

--- junior/advice-generator-app-main/app.js
+++ junior/advice-generator-app-main/app.js
@@ -1,8 +1,18 @@
-fetch("https://api.adviceslip.com/advice").then((response) => {
-    response.json().then(function(slip){
-        console.log(slip.slip.advice)
+const button = document.querySelector("#dice-btn");
+
+button.addEventListener("click", () => {
+    fetch("https://api.adviceslip.com/advice").then(response => {
+        response.json().then(data => {
+            const adviceID = document.querySelector("#advice-id");
+            const adviceText = document.querySelector("#advice-text");
+
+
+            adviceID.textContent = data.slip.id
+            adviceText.textContent = data.slip.advice
+        })
     })
 })
+
 
 
 
