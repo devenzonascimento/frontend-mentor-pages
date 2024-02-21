10fa7ec7f56e08bf100ed9696f9ac8ca9ff8761d
Enzo Nascimento <dev.enzody@gmail.com>
2024-02-21 15:33:47 -0300

    ATT

--- junior/crowdfunding-product-page-main/script.js
+++ junior/crowdfunding-product-page-main/script.js
@@ -10,3 +10,25 @@ openButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
     modal.close()
 })
+
+const radioInputs = document.querySelectorAll(".radio-input");
+
+radioInputs.forEach(radioInput => {
+    radioInput.addEventListener("change", () => {
+        document.querySelectorAll(".input-container").forEach(container => {
+            container.classList.remove("active")
+        })
+        
+        const inputContainer = radioInput.closest(".input-container");
+        inputContainer.classList.add("active")
+
+        document.querySelectorAll(".pledge-input").forEach(pledge => {
+            pledge.style.display = "none"
+        })
+
+        /*  O ERRO ESTÁ AQUI, NA HORA DE ENCONTRAR O ELEMENTO MAIS PROXIMO
+        const pledgeInput = radioInput.closest(".pledge-input");
+        pledgeInput.style.display = "grid"
+        */
+    })
+})
