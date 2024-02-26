854cc1138502b66dd516dd9a52da557622260074
Enzo Nascimento <dev.enzody@gmail.com>
2024-02-26 12:58:45 -0300

    finish project

--- junior/crowdfunding-product-page-main/script.js
+++ junior/crowdfunding-product-page-main/script.js
@@ -1,34 +1,73 @@
+const selectionModal = document.querySelector(".selection-modal")
+
 const openButton = document.querySelector("#back-btn");
-const closeButton = document.querySelector("#icon-close-modal");
-
-const modal = document.querySelector(".selection-modal")
-
 openButton.addEventListener("click", () => {
-    modal.showModal()
+    selectionModal.showModal()
 })
-
+const closeButton = document.querySelector("#icon-close-modal");
 closeButton.addEventListener("click", () => {
-    modal.close()
+    selectionModal.close()
 })
 
-const radioInputs = document.querySelectorAll(".radio-input");
 
-radioInputs.forEach(radioInput => {
-    radioInput.addEventListener("change", () => {
-        document.querySelectorAll(".input-container").forEach(container => {
-            container.classList.remove("active")
-        })
-        
-        const inputContainer = radioInput.closest(".input-container");
-        inputContainer.classList.add("active")
-
-        document.querySelectorAll(".pledge-input").forEach(pledge => {
-            pledge.style.display = "none"
-        })
-
-        /*  O ERRO ESTÁ AQUI, NA HORA DE ENCONTRAR O ELEMENTO MAIS PROXIMO
-        const pledgeInput = radioInput.closest(".pledge-input");
-        pledgeInput.style.display = "grid"
-        */
+const continueButtons = document.querySelectorAll(".continue-btn");
+continueButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const successModal = document.querySelector(".success-modal")
+        successModal.showModal()
+        selectionModal.close()
     })
 })
+
+
+const radioInputs = document.querySelectorAll(".radio-input");
+radioInputs.forEach(radioInput => {
+    radioInput.addEventListener("change", () => {
+        
+        removeInputStyles(document.querySelectorAll(".input-container"))
+        // Adiciona o efeito de Borda no Input
+        const inputContainer = radioInput.closest(".input-container");
+        inputContainer.classList.add("border-active")
+
+        hideInputContainers(document.querySelectorAll(".pledge-input"))
+        // Mostra o Input
+        const pledgeInput = radioInput.parentNode.nextElementSibling;
+        pledgeInput.style.display = "grid"
+        
+    })
+})
+function removeInputStyles(inputElement) {
+    inputElement.forEach(input => {
+        input.classList.remove("border-active")
+    })    
+}
+function hideInputContainers(inputElement) {
+    inputElement.forEach(input => {
+        input.style.display = "none"
+    })
+}
+
+
+const iconMenu = document.querySelector(".icon-menu")
+iconMenu.addEventListener("click", () => {  
+    iconMenu.classList.toggle("close")
+    if (iconMenu.classList.contains("close")) {
+        document.querySelector("nav").style.display = "flex"
+        createBackdrop()
+    } else {
+        document.querySelector("nav").style.display = "none"
+        removeBackdrop()
+    }
+})
+function createBackdrop() {
+    if (!document.querySelector(".backdrop")) {
+        const backdrop = document.createElement("div")
+        backdrop.classList.add("backdrop")
+        document.body.appendChild(backdrop)
+    }
+}
+function removeBackdrop() {
+    if (document.querySelector(".backdrop")) {
+        document.querySelector(".backdrop").remove()
+    }
+}
