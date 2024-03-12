a0278cadd5b59f4f5c6e1d9c9b35b6a498e62190
Enzo Nascimento <dev.enzody@gmail.com>
2024-03-12 10:51:49 -0300

    update

--- template-cpu-simulator/funcional.js
+++ template-cpu-simulator/funcional.js
@@ -1,7 +1,3 @@
-document.getElementById("step").addEventListener("click", () => {
-    executarProximoPasso(main)
-})
-
 let memory = {
     "00000000": "10010001",
     "00000001": "00111111",
@@ -12,6 +8,7 @@ let memory = {
     "00000110": "00000000",
     "00000111": "00000000",
 }
+
 let contador = 0
 const pc = document.getElementById("pc");
 const mar = document.getElementById("mar");
@@ -24,35 +21,38 @@ let operand = "";
 
 
 let currentStep = 0;
+
 function executarProximoPasso(operationStep) {
-    if (currentStep < operationStep.length) {
-        operationStep[currentStep]();
-        currentStep++
-    }
+
+
+    //console.log(`passo(${currentStep}) ==>${operationStep[currentStep]}`)
+    operationStep[currentStep]();
+    currentStep++
+
 }
-const search = [
+
+let main = [
     () => pc.textContent = toBinary(contador),
     () => mar.textContent = pc.textContent,
     () => mdr.textContent = memory[mar.textContent],
     () => cir.textContent = mdr.textContent,
     () => pc.textContent = toBinary(contador+=1),
+    () => console.log(cir.textContent),
     () => decode(cir.textContent),
+    //() => main.concat(inputInstruction)
+    //() => console.log(main)
 ];
-function instructionExecute(array) {
-    main = main.concat(array)
-}
-let main = [
-    () => instructionExecute(search)
-];
-
 
 function decode(cir) {
 
+
     opcode = cir.substring(0, 4)
     operand = cir.substring(4, 8)
+    console.log(`OPCODE: ${opcode} | OPERAND: ${operand}`)
 
     switch (opcode) {
         case "0000":
+            console.log("Executou END")
             return false
 
         case "0001":
@@ -66,7 +66,7 @@ function decode(cir) {
             instructionExecute(storeInstruction)
             break;
         case "0101":
-            instructionExecute(loadInstruction)
+        instructionExecute(loadInstruction)
             break;
         case "1001":
             operand === "0001" ? instructionExecute(inputInstruction) : false
@@ -77,43 +77,76 @@ function decode(cir) {
     }
 }
 
+function instructionExecute(array) {
+    //for (let i = 0; i < array.length; i++) {
+    //    main.push(array[i])
+    //}
+    main = main.concat(array)
+}
+
+const search = [
+    () => pc.textContent = toBinary(contador),
+    () => mar.textContent = pc.textContent,
+    () => mdr.textContent = memory[mar.textContent],
+    () => cir.textContent = mdr.textContent,
+    () => pc.textContent = toBinary(contador+=1),
+    () => console.log(cir.textContent),
+    () => decode(cir.textContent),
+];
+
 const addInstruction = [
+    () => console.log("Inicio ADD"),
     () => mar.textContent = operand.padStart(8, '0'),
     () => mdr.textContent = memory[mar.textContent],
     () => acc.textContent = toBinary(toDecimal(acc.textContent) + toDecimal(mdr.textContent)),
+    () => console.log("Fim ADD"),
     () => instructionExecute(search)
 ];
 
 const subInstruction = [
+    () => console.log("Inicio SUB"),
     () => mar.textContent = operand.padStart(8, '0'),
     () => mdr.textContent = memory[mar.textContent],
     () => acc.textContent = toBinary(toDecimal(acc.textContent) - toDecimal(mdr.textContent)),
+    () => console.log("Fim SUB"),
     () => instructionExecute(search)
 ];
 
 const storeInstruction = [
+    () => console.log("Inicio STORE"),
     () => mar.textContent = operand.padStart(8, '0'),
     () => memory[mar.textContent] = acc.textContent,
+    () => console.log("Fim STORE"),
     () => instructionExecute(search)
 ]
 
 const loadInstruction = [
+    () => console.log("Inicio LOAD"),
     () => mar.textContent = operand.padStart(8, '0'),
     () => mdr.textContent = memory[mar.textContent],
     () => acc.textContent = mdr.textContent,
+    () => console.log("Fim LOAD"),
     () => instructionExecute(search)
 ]
 
 const inputInstruction = [
+    () => console.log("Inicio INPUT"),
     () => acc.textContent = toBinary(prompt("Informe um valor")),
+    () => console.log("Fim INPUT"),
     () => instructionExecute(search)
 ]
 
 const outputInstruction = [
+    () => console.log("Inicio OUTPUT"),
     () => alert(acc.textContent),
+    () => console.log("Fim OUTPUT"),
     () => instructionExecute(search)
 ]
 
+
+document.getElementById("step").addEventListener("click", () => {
+    executarProximoPasso(main)
+})
 
 function toBinary(num) {
     return Number(num).toString(2).padStart(8, '0')
@@ -121,3 +154,16 @@ function toBinary(num) {
 function toDecimal(num) {
     return parseInt(num, 2)
 }
+
+/*
+0000	0000	    End
+0001	address	    Add
+0010	address	    Subtract
+0011	address	    Store
+0101	address	    Load
+0110	address	    Branch Always
+0111	address	    Branch if ACC = 0
+1000	address	    Branch if ACC >= 0
+1001	0001	    Input
+1001	0010	    Output
+*/
