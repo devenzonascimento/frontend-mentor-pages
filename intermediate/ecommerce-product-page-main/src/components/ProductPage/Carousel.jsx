acbfbdfdd81697a2f505dd3d2842967cf4134035
Enzo Nascimento <dev.enzody@gmail.com>
2024-04-01 19:11:25 -0300

    carousel ok

--- intermediate/ecommerce-product-page-main/src/components/ProductPage/Carousel.jsx
+++ intermediate/ecommerce-product-page-main/src/components/ProductPage/Carousel.jsx
@@ -1,32 +1,42 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const productImages = [
+  "src/assets/image-product-1.jpg",
+  "src/assets/image-product-2.jpg",
+  "src/assets/image-product-3.jpg",
+  "src/assets/image-product-4.jpg",
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "next":
+      return { index: (state.index + 1) % productImages.length };
+    case "previous":
+      return {
+        index: (state.index + productImages.length - 1) % productImages.length,
+      };
+    default:
+      return state;
+  }
+};
 
 const Carousel = () => {
-
-    const productImages = [
-        "src/assets/image-product-1.jpg",
-        "src/assets/image-product-2.jpg",
-        "src/assets/image-product-3.jpg",
-        "src/assets/image-product-4.jpg",
-    ]
+  const [state, dispatch] = useReducer(reducer, { index: 0 });
 
   return (
     <div className="carousel-container">
-      <img
-        src={productImages[0]}
-        alt=""
-        className="current-image"
-      />
-      <button className="previous-icon carousel-icon">
-        <img
-          src="src/assets/icon-previous.svg"
-          alt=""
-        />
+      <img src={productImages[state.index]} alt="" className="current-image" />
+      <button
+        className="previous-icon carousel-icon"
+        onClick={() => dispatch({ type: "previous" })}
+      >
+        <img src="src/assets/icon-previous.svg" alt="" />
       </button>
-      <button className="next-icon carousel-icon">
-        <img
-          src="src/assets/icon-next.svg"
-          alt=""
-        />
+      <button
+        className="next-icon carousel-icon"
+        onClick={() => dispatch({ type: "next" })}
+      >
+        <img src="src/assets/icon-next.svg" alt="" />
       </button>
     </div>
   );
